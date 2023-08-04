f17f9c778fdd254f403c60e32dae293d80d2f456
EzekTec <stephen.ezekwem@ezektec.com>
2023-08-04 03:12:34 -0600

    Initial commit of the Resend api service in rust programming language

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use anyhow::{anyhow, Result};
+use reqwest::{Response, Error};
+use serde::{Deserialize, Serialize};
+use std::env;
+use dotenv;
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Payload {
+    from: String,
+    to: String,
+    subject: String,
+    html: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResendPayload {
+    uri: String,
+    authorization: String,
+    header: String,
+    body: Payload,
+}
+
+impl ResendPayload {
+    pub fn new(uri: String, auth: String, head: String, body: Payload) -> Self {
+        Self {
+            uri,
+            authorization: auth,
+            header: head,
+            body,
+        }
+    }
+
+    pub async fn send_email(&self) -> Result<String> {
+        let client = reqwest::Client::new();
+        let res: reqwest::Response = client
+            .post(&self.uri)
+            .header("Authorization", &self.authorization)
+            .header("Content-Type", &self.header)
+            .json(&serde_json::json!(&self.body))
+            .send()
+            .await?;
+        
+        // println!("RESPONSE: {:#?}", &res); // NOTE: Please remove this line as it's only here
+        // for testing purposes.
+
+        match res.status() {
+            reqwest::StatusCode::OK => {
+                Ok("Success: Email sent.".to_owned())
+            },
+            reqwest::StatusCode::UNAUTHORIZED => {
+                Err(anyhow!("Error: UNAUTHORIZED"))
+            },
+            _ => {
+                Err(anyhow!("Error: All other ERRORS!."))
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    dotenv::dotenv().ok();
+    
+    let payload = Payload {
+        from: "onboarding@resend.dev".to_owned(),
+        to: env::var("TO_EMAIL")?.to_owned(),
+        subject: "Additional from Resend".to_owned(),
+        html: "<p>Congrats again on sending another <strong>custom email</strong>!</p>".to_owned(),
+    };
+
+    let payload_to_send = ResendPayload::new(
+        "https://api.resend.com/emails".to_owned(),
+        format!("Bearer {}", env::var("AUTHORIZATION_API_KEY")?),
+        "application/json".to_owned(),
+        payload,
+    );
+
+    let response = payload_to_send.send_email().await?;
+
+    println!("{:#?}", response);
+
+    Ok(())
+}
