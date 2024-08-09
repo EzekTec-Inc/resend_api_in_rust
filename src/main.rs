34d218b385877da0ef1f1ca712a4868c0e784ba3
EzekTec <stephen.ezekwem@ezektec.com>
2024-08-08 18:16:40 -0600

    refactored for ease of use.

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,60 @@ use serde::{Deserialize, Serialize};
 use std::env;
 
 #[derive(Debug, Clone, Serialize)]
-pub struct ResendResponse {
+pub struct ResendSDKResponse {
     message: String,
     content: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Payload {
-    from: String,
-    to: String,
-    subject: String,
-    html: String,
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct EmailPayload {
+    _from: String,
+    _to: String,
+    _subject: String,
+    _html: String,
+}
+impl EmailPayload {
+    pub fn new(from: String, to: String, subject: String, html: String) -> Self {
+        Self {
+            _from: from,
+            _to: to,
+            _subject: subject,
+            _html: html,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ResendPayload {
+pub struct ResendSDK {
     uri: String,
     authorization: String,
     header: String,
-    body: Payload,
+    body: EmailPayload,
 }
 
-impl ResendPayload {
-    pub fn new(uri: String, auth: String, head: String, body: Payload) -> Self {
+impl ResendSDK {
+    /// Initializes the SDK.
+    pub fn init(uri: String, header: String, auth: String) -> Self {
         Self {
             uri,
             authorization: auth,
-            header: head,
-            body,
+            header,
+            body: EmailPayload::default(),
+        }
+    }
+    /// Sets the email payload.
+    pub fn with_email_payload(&self, email_payload: EmailPayload) -> Self {
+        Self {
+            body: email_payload,
+            ..self.clone()
         }
     }
 
+    /// Returns the send email of this [`ResendSDK`].
+    ///
+    /// # Errors [`reqwest::Error`]
+    ///
+    /// This function will return an error if the api-endpoint is not reachable.
     pub async fn send_email(&self) -> Result<String> {
         let client = reqwest::Client::new();
         let res: reqwest::Response = client
@@ -44,13 +67,10 @@ impl ResendPayload {
             .send()
             .await?;
 
-        // println!("RESPONSE: {:#?}", &res); // NOTE: Please remove this line as it's only here
-        // for testing purposes.
-
         match res.status() {
             reqwest::StatusCode::OK => {
                 let response: serde_json::Value = res.json().await?;
-                let resend_response = ResendResponse {
+                let resend_response = ResendSDKResponse {
                     message: "Success: Email sent.".to_owned(),
                     content: response.to_string(),
                 };
@@ -69,17 +89,19 @@ type ServiceResponse = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 async fn main() -> ServiceResponse {
     dotenv::dotenv().ok();
 
-    let payload = ResendPayload::new(
-        "https://api.resend.com/emails".to_owned(),
-        env::var("AUTHORIZATION_API_KEY")?,
-        "application/json".to_owned(),
-        Payload {
-            from: "RocketsRus <delivered@resend.dev>".to_owned(),
-            to: env::var("TO_EMAIL")?,
-            subject: "Demo email from Resend".to_owned(),
-            html: "<p>Congrats on sending a <strong>custom email</strong> using <strong>Resend</strong> api</p>"
-                .to_owned(),
-        },
+    // NOTE: Please modify these variables to suite your needs, or adopt a different approach.
+    let api_service_uri = "https://api.resend.com/emails".to_owned();
+    let api_header = "application/json".to_owned();
+    let auth_api_key = env::var("AUTHORIZATION_API_KEY")?;
+
+    let email_to = env::var("TO_EMAIL")?;
+    let email_from = "RocketsRus <delivered@resend.dev>".to_owned();
+    let email_subject = "Demo email from Resend".to_owned();
+    let email_html = "<p>Congrats on sending a <strong>custom email</strong> using <strong>Resend</strong> api</p>"
+                .to_owned();
+
+    let payload = ResendSDK::init(api_service_uri, api_header, auth_api_key).with_email_payload(
+        EmailPayload::new(email_from, email_to, email_subject, email_html),
     );
 
     let response = payload.send_email().await?;
@@ -98,22 +120,26 @@ mod tests {
     async fn test_send_email() {
         dotenv::dotenv().ok();
 
-        let test_payload = ResendPayload::new(
-            "https://api.resend.com/emails".to_owned(),
-            env::var("AUTHORIZATION_API_KEY").unwrap(),
-            "application/json".to_owned(),
-            Payload {
-                from: "RocketsRus <delivered@resend.dev>".to_owned(),
-                to: env::var("TO_EMAIL").unwrap(),
-                subject: "Demo email from Resend".to_owned(),
-                html: "<p>Congrats on sending a <strong>custom email</strong> using <strong>Resend</strong> api</p>"
-                    .to_owned(),
-            },
-        );
+        let test_api_service_uri = "https://api.resend.com/emails".to_owned();
+        let test_api_header = "application/json".to_owned();
+        let test_auth_api_key = env::var("AUTHORIZATION_API_KEY").unwrap();
+
+        let test_email_to = env::var("TO_EMAIL").unwrap();
+        let test_email_from = "RocketsRus <delivered@resend.dev>".to_owned();
+        let test_email_subject = "Demo email from Resend".to_owned();
+        let test_email_html = "<p>Congrats on sending a <strong>test email from your unit test [test_send_email()] function</strong> using <strong>Resend</strong> api</p>"
+                    .to_owned();
+        let test_payload =
+            ResendSDK::init(test_api_service_uri, test_api_header, test_auth_api_key)
+                .with_email_payload(EmailPayload::new(
+                    test_email_from,
+                    test_email_to,
+                    test_email_subject,
+                    test_email_html,
+                ));
 
         let test_response = test_payload.send_email().await.unwrap();
 
-        //println!("{:#?}", test_response);
         assert!(!test_response.is_empty());
     }
 }
