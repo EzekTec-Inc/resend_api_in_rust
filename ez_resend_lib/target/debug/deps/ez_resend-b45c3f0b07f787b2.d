6c7ed9d3a4758bc1e10b7eb8f9a07b8ad68c3c88
EzekTec-Inc <stephen.ezekwem@ezektec.com>
2024-08-16 13:20:54 -0600

    updated the unit-test to use async. Thanks to 'Training 4 Programmers' for the code review.

--- ez_resend_lib/target/debug/deps/ez_resend-b45c3f0b07f787b2.d
+++
@@ -1,5 +0,0 @@
-/home/engr-uba/Downloads/02 Rust-project/resend_api_in_rust/ez_resend/target/debug/deps/libez_resend-b45c3f0b07f787b2.rmeta: src/lib.rs
-
-/home/engr-uba/Downloads/02 Rust-project/resend_api_in_rust/ez_resend/target/debug/deps/ez_resend-b45c3f0b07f787b2.d: src/lib.rs
-
-src/lib.rs:
